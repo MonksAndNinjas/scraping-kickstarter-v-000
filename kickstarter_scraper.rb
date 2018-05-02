fc15a4509782ea3fcc6943bf633c238252d4a884
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-02 05:14:17 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,9 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
-  kickstarter
 end
-  binding.pry
 
 create_project_hash
+
+
+binding.pry
