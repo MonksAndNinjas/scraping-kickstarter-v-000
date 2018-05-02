c9bec0d4cf309597e81f6c6f48f14a8f5685d03a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-02 05:16:26 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,15 +1,10 @@
 #require 'libraries/modules'
-
 require 'pry'
 
 def create_project_hash
+  # write your code here
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
 end
-
-create_project_hash
-
-
-binding.pry
