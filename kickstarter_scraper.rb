bebe7a5a198ddeaa6b75abdfb27b7d5d85cb5615
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-02 06:02:25 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,14 +12,18 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+ 
   projects = {}
-
+ 
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
-
+ 
   # return the projects hash
   projects
 end
+
+create_project_hash
+
+binding.pry
